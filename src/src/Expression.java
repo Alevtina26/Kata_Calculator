f88c7a0257cf5d8cd58912ca2dd2673c4083f55d
Alevtina26 <safonova263519@yandex.ru>
2024-06-14 12:23:23 +0300

    Исправлены замечания
    Удалены модификаторы доступа добавленных классов и неиспользуемый код.

---
+++ src/src/Expression.java
@@ -0,0 +1,17 @@
+class Expression {
+
+    public static int count(int x, int y, String sign) {
+        switch (sign) {
+            case "+":
+                return x + y;
+            case "-":
+                return x - y;
+            case "*":
+                return x * y;
+            case "/":
+                return x / y;
+            default:
+                return 0;
+        }
+    }
+}
---
+++ src/src/Main.java
@@ -0,0 +1,62 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+class Main {
+
+    private static final List<String> SINGS = Arrays.asList("+", "-", "/", "*");
+    private static final Predicate<String> IS_SING = SINGS::contains;
+
+
+    public static String calc(String input) throws IllegalAccessException {
+        var arrayOfExpression = input.split(" ");
+
+        checkExpression(arrayOfExpression);
+        var sign = checkAndGetSing(arrayOfExpression[1]);
+        var first = arrayOfExpression[0];
+        var second = arrayOfExpression[2];
+
+        int firstNumber;
+        int secondNumber;
+        boolean isArabDigit;
+        try {
+            firstNumber = Integer.parseInt(first);
+            secondNumber = Integer.parseInt(second);
+            isArabDigit = true;
+        } catch (Exception e) {
+            firstNumber = Converter.toArab(first);
+            secondNumber = Converter.toArab(second);
+            isArabDigit = false;
+        }
+
+        checkDigitRange(firstNumber, secondNumber);
+
+        var result = Expression.count(firstNumber, secondNumber, sign);
+        if (!isArabDigit) {
+            if (result <= 0) {
+                throw new IllegalArgumentException("Результат выражения с римскими числами не может быть меньше или равен нулю.");
+            }
+        }
+        return isArabDigit ? String.valueOf(result) : Converter.toRome(result);
+
+    }
+
+    private static void checkDigitRange(int firstNumber, int secondNumber) {
+        if (firstNumber < 1 || firstNumber > 10 || secondNumber < 1 || secondNumber > 10) {
+            throw new IllegalArgumentException("Диапазон чисел должен быть от 0 до 10.");
+        }
+    }
+
+    private static String checkAndGetSing(String s) {
+        if (IS_SING.test(s)) {
+            return s;
+        }
+        throw new IllegalArgumentException("Введен неверный знак");
+    }
+
+    private static void checkExpression(String[] arrayOfExpression) {
+        if (arrayOfExpression.length != 3) {
+            throw new IllegalArgumentException("Неправильно введено выражение");
+        }
+    }
+}
